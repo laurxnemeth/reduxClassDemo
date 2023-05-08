4e20130d2ea970a40ccc5f724f1fe26dd15ba925
laurxnemeth <lauranemeth.pr@gmail.com>
2023-05-08 12:42:06 -0400

    Add Redux Demo

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
 import animeSlice from "./animeSlice";
 
+console.log(animeSlice);
+
 export const store = configureStore({
 	reducer: {
 		counter: counterSlice,
